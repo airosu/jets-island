b8ffdbd588eb9d6d55e57ac171ae3077b6e229ff
Alexandru <alexandru.ionut.rosu@gmail.com>
2022-07-10 12:50:52 +0300

    build: add theme support to emotion styled components

--- src/components/templates/BaseTemplate/BaseTemplate.styles.ts
+++ src/components/templates/BaseTemplate/BaseTemplate.styles.ts
@@ -1,1 +1,6 @@
-export {}
+import styled from '@emotion/styled'
+
+export const StyledWrapper = styled.div`
+    background-color: ${({ theme }) => theme.palette.secondary.main};
+    color: ${({ theme }) => theme.palette.warning.main};
+`
